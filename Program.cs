4785811a2a420b21abd7427ce4cf4db7111c1a9f
Priyanka Thakur <thakurp@usf.edu>
2020-02-04 20:34:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise3_2
+{
+    class Car
+    {
+        public string color = "red";
+
+        static void Main(string[] args)
+        {
+            Car myobj = new Car();
+            Console.WriteLine(myobj.color);
+        }
+    }
+
+}
